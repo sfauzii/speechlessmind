c3313e0fe37689f5b65002a15ff998a580547baf
sfauzii <sjahrulfauzii@gmail.com>
2024-02-15 13:01:08 +0700

    feat: migration table blog
    Closes #6

--- database/migrations/2023_06_24_051825_create_filament_blog_tables.php
+++ database/migrations/2023_06_24_051825_create_filament_blog_tables.php
@@ -20,6 +20,8 @@ return new class () extends Migration {
             $table->boolean('is_visible')->default(false);
             $table->string('seo_title', 60)->nullable();
             $table->string('seo_description', 160)->nullable();
+            
+            $table->date('published_at')->nullable();
             $table->timestamps();
         });
 
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,11 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,10 +18,28 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        \App\Models\User::factory()->create([
-            'name' => 'Kevin',
-            'email' => 'kevin@gmail.com',
-            'password' => bcrypt('password')
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Kevin',
+        //     'email' => 'kevin@gmail.com',
+        //     'password' => bcrypt('password')
+        // ]);
+
+        $user1 = User::factory()->create([
+            'name' => 'Admin',
+            'email' => 'admin@admin.com',
         ]);
+
+        $user2 = User::factory()->create([
+            'name' => 'User',
+            'email' => 'user@user.com',
+        ]);
+
+        $role = Role::create(['name' => 'superadmin']);
+        $user1->assignRole($role);
+
+        $role = Role::create(['name' => 'user']);
+        $user2->assignRole($role);
+
+
     }
 }
