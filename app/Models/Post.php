122bedb9d27afd5a98c473706ba5740f9335379c
sfauzii <sjahrulfauzii@gmail.com>
2024-02-15 12:55:35 +0700

    refactor: modify featured
    Closes #3

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,12 +2,18 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Spatie\Tags\HasTags;
+use Filament\Models\Contracts\FilamentUser;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Post extends \Stephenjude\FilamentBlog\Models\Post
 {
     use HasFactory;
+
+    
+
+  
+
 
     public function scopeFilter($query, array $filters = []): void
     {
@@ -33,8 +39,64 @@ class Post extends \Stephenjude\FilamentBlog\Models\Post
         );
     }
 
+    public static function getRelatedPosts(Post $post, int $count = 4, bool
+    $shuffle = false)
+    {
+        $relatedPosts = collect();
+        $allPosts = Post::all();
+
+        foreach($allPosts as $otherPost) {
+            if($otherPost->id != $post->id){
+                similar_text($otherPost->title, $post->title, $percent);
+
+                if($percent >= 1) {
+                    $relatedPosts->push($otherPost);
+
+                    if ($relatedPosts->count() == $count) {
+                        return $relatedPosts->shuffle();
+                    }
+                }
+            }           
+        }
+
+        if ($relatedPosts->count() < $count) {
+           $sameCategoryPosts = Post::where('blog_category_id', 
+           $post->blog_category_id)
+                ->whereNot('id', $post->id)
+                ->whereNotIn('id', $relatedPosts->pluck('id')->toArray())
+                ->take($count - $relatedPosts->count())           
+                ->get();
+
+           $relatedPosts = $relatedPosts->concat($sameCategoryPosts);
+        }
+
+        if ($relatedPosts->count() < $count) {
+            $randomPosts = Post::whereNot('id', $post->id)
+                ->whereNotIn('id', $relatedPosts->pluck('id')->toArray())
+                ->inRandomOrder()
+                ->take($count - $relatedPosts->count())
+                ->get();
+        }
+
+
+        return $shuffle ? $relatedPosts->shuffle() : $relatedPosts;
+        
+
+
+    }
+
+    
+
+
     public function getRouteKeyName(): string
     {
         return 'slug';
     }
+
+    // public function posts()
+    // {
+
+    //     return $this->hasMany(Post::class, 'blog_author_id', 'id');
+
+    // }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -2,22 +2,51 @@
 
 namespace App\Models;
 
+use Laravel\Sanctum\HasApiTokens;
+use Laravel\Jetstream\HasProfilePhoto;
+use Spatie\Permission\Traits\HasRoles;
+use Illuminate\Notifications\Notifiable;
+// use Filament\Tables\Columns\Layout\Panel;
+use Filament\Tables\Columns\Layout\Panel;
+use Filament\Models\Contracts\FilamentUser;
+use Laravel\Fortify\TwoFactorAuthenticatable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Fortify\TwoFactorAuthenticatable;
-use Laravel\Jetstream\HasProfilePhoto;
-use Laravel\Sanctum\HasApiTokens;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     use HasApiTokens;
     use HasFactory;
     use HasProfilePhoto;
     use Notifiable;
     use TwoFactorAuthenticatable;
+    use HasRoles;
 
+    const ROLE_ADMIN = 'ADMIN';
+    const ROLE_EDITOR = 'EDITOR';
+    const ROLE_USER = 'USER';
+
+    const ROLE_DEFAULT = self::ROLE_USER;
+
+    const ROLES = [
+        self::ROLE_ADMIN => 'Admin',
+        self::ROLE_EDITOR => 'Editor',
+        self::ROLE_USER => 'User',
+    ];
+
+    // public function canAccessPanel(Panel $panel): bool
+    // {
+    //    return $this->isAdmin() || $this->isEditor();
+    // }
+
+    public function isAdmin(){
+       return $this->role === self::ROLE_ADMIN;
+    }
+     
+    public function isEditor(){
+        return $this->role === self::ROLE_EDITOR;
+    }
     /**
      * The attributes that are mass assignable.
      *
@@ -27,6 +56,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role'
     ];
 
     /**
@@ -50,6 +80,16 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
+
+    
+
+    public function canAccessFilament(): bool
+        
+    {
+        return $this->can('view-admin', User::class);
+        // return $this->hasRole(['superadmin', 'writer', 'admin']);
+    }
+
     /**
      * The accessors to append to the model's array form.
      *
@@ -58,4 +98,8 @@ class User extends Authenticatable
     protected $appends = [
         'profile_photo_url',
     ];
+
+    
+
+    
 }
