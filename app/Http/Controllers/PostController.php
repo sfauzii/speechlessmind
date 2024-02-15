4cfae4576056f8f00c4d591a9603d1d37bcc6c8f
sfauzii <sjahrulfauzii@gmail.com>
2024-02-15 12:52:02 +0700

    refactor: error bug

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,13 +4,24 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Post;
+use App\Models\Video;
+// use App\Models\Author;
 
 class PostController extends Controller
 {
+    
     public function welcome()
     {
+        // return view('welcome', [
+        //     'posts' => Post::latest()->paginate(9)
+        // ]);
+
+        $posts = Post::latest()->paginate(9);
+        $videos = Video::all();
+        
         return view('welcome', [
-            'posts' => Post::latest()->paginate(9)
+            'posts' => $posts,
+            'videos' => $videos
         ]);
     }
 
@@ -24,8 +35,41 @@ class PostController extends Controller
 
     public function show(Post $post)
     {
+        $sameAuthorPosts = collect();
+        $count = 4;
+
+        $sameAuthorPosts = $sameAuthorPosts->concat(
+            $post
+                ->where('blog_author_id', $post->author->id)
+                ->whereNot('id', $post->id)
+                ->latest()
+                ->limit($count)
+                ->get()
+        );
+
+        // Assuming you have an Author instance or ID
+        // $author = Post::find($post); // Replace $authorId with the actual author ID
+
+        // // Get the post count
+        // $postCount = $author->posts()->count();
+
+        // $author = Post::find($post);
+
+        // if (!$author) {
+        //     abort(404); // or handle the case where the author is not found
+        // }
+
+        //  $postCount = $author->posts()->count();
+
         return view('posts.show', [
             'post' => $post,
+            'sameAuthorPosts' => $sameAuthorPosts,
+            'related_posts' => Post::getRelatedPosts($post, shuffle: true),
+            
         ]);
     }
+
+    
+
+    
 }
