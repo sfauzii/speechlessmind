c0a085751785940634c7facf34407fc76c1d1751
sfauzii <sjahrulfauzii@gmail.com>
2024-02-17 11:48:32 +0700

    refactor(controller): post controller feature
    Closes #13

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,14 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Hero;
 use Illuminate\Http\Request;
 use App\Models\Post;
 use App\Models\Video;
+
+use Illuminate\Support\Facades\Storage;
 // use App\Models\Author;
 
 class PostController extends Controller
 {
-    
     public function welcome()
     {
         // return view('welcome', [
@@ -18,10 +20,12 @@ class PostController extends Controller
 
         $posts = Post::latest()->paginate(9);
         $videos = Video::all();
-        
+        $heroes = Hero::all();
+
         return view('welcome', [
             'posts' => $posts,
-            'videos' => $videos
+            'videos' => $videos,
+            'heroes' => $heroes,
         ]);
     }
 
@@ -29,8 +33,36 @@ class PostController extends Controller
     {
         return view('posts.index', [
             'posts' => Post::filter(request(['search', 'category', 'author']))->get(),
-            'categories' => \Stephenjude\FilamentBlog\Models\Category::all()
+            'categories' => \Stephenjude\FilamentBlog\Models\Category::all(),
         ]);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'hero_pdf' => 'required|mimes:pdf|max:2048', // Misalnya, batasi file hanya menerima PDF maksimal 2MB
+        ]);
+
+        $file = $request->file('hero_pdf');
+
+        // Simpan file di dalam penyimpanan yang diinginkan, misalnya di dalam direktori 'public/uploads'
+        $path = $file->store('uploads', 'public');
+
+        // Sekarang Anda bisa menyimpan $path ke dalam database atau melakukan apa pun yang diperlukan dengan informasi file tersebut
+
+        return back()->with('success', 'File has been uploaded successfully.');
+
+
+        $request->validate([
+            'banner_hero' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048', // Validasi file gambar
+        ]);
+
+        $file = $request->file('banner_hero');
+
+        // Simpan file di dalam penyimpanan default Filament
+        $path = Storage::putFile('public', $file);
+
+        return back()->with('success', 'File has been uploaded successfully.');
     }
 
     public function show(Post $post)
@@ -44,7 +76,7 @@ class PostController extends Controller
                 ->whereNot('id', $post->id)
                 ->latest()
                 ->limit($count)
-                ->get()
+                ->get(),
         );
 
         // Assuming you have an Author instance or ID
@@ -65,11 +97,6 @@ class PostController extends Controller
             'post' => $post,
             'sameAuthorPosts' => $sameAuthorPosts,
             'related_posts' => Post::getRelatedPosts($post, shuffle: true),
-            
         ]);
     }
-
-    
-
-    
 }
